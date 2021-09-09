c83e89df6f6d6c37231b10ae47ff5ddbc81cf71d
Michel Oliveira <michelsantosoliveira@hotmail.com>
2021-09-09 11:09:13 -0300

    Requisitos 6, 7 e tenta resolver o requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,47 @@
-// implement AddMovie component here
+import React from 'react';
+
+// Crie um componente chamado `<AddMovie />`
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(target) {
+    const { name } = target;
+    this.setState({
+      [name]: target.value,
+    });
+  }
+
+  render() {
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <div>
+        {/* Renderize um formulário dentro de `<AddMovie />` */}
+        <form data-testid="add-movie-form">
+          <label htmlFor="title" data-testid="title-input-label">
+            Título
+            <input
+              data-testid="title-input"
+              value={ title }
+              onChange={ this.handleChange }
+            />
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
